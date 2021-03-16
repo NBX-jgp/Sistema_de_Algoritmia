b430fd8449d49cc787281ebeee39ab4cf1d7afb5
Santiago Quintero Pérez <69019228+NBX-jgp@users.noreply.github.com>
2021-03-16 14:14:12 -0500

    Inicio

---
+++ algoritmos/ejercicio1/codigo.js
@@ -0,0 +1,6 @@
+function multiplicar(){
+  var v1 = parseInt(document.getElementById("n1").value);
+  var v2 = parseInt(document.getElementById("n2").value);
+  document.getElementById("respuesta").innerHTML = "<font color=red>" + (v1 * v2); + "</font>"
+
+} 
